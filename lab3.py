6cfabde76814cf2223c0bfbc872c976239a3600e
Andrew <spanoproandrei2245@gmail.com>
2026-03-21 17:23:47 +0200

    politika LRU i LFU

--- lab3.py
+++ lab3.py
@@ -2,9 +2,26 @@ import time
 import functools
 from collections import OrderedDict, defaultdict
 
-def memoize(funktsiya, rozmir_keshu=None):
+def memoize(funktsiya, rozmir_keshu=None, polityka='lru'):
     kesh = OrderedDict()
     lichylnyk_zvertanj = defaultdict(int)
+    chas_stvorennya = {}
+
+    def _zastosuvatyEviktsiynu():
+        if rozmir_keshu is None or len(kesh) < rozmir_keshu:
+            return
+
+        if polityka == 'lru':
+            kesh.popitem(last=False)
+
+        elif polityka == 'lfu':
+            naymenshe_zvertanj = min(lichylnyk_zvertanj[k] for k in kesh)
+            for klyuch in list(kesh.keys()):
+                if lichylnyk_zvertanj[klyuch] == naymenshe_zvertanj:
+                    kesh.pop(klyuch)
+                    lichylnyk_zvertanj.pop(klyuch, None)
+                    chas_stvorennya.pop(klyuch, None)
+                    break
 
     @functools.wraps(funktsiya)
     def obgortka(*arhy, **kvarhy):
@@ -12,12 +29,17 @@ def memoize(funktsiya, rozmir_keshu=None):
 
         if klyuch_keshu in kesh:
             lichylnyk_zvertanj[klyuch_keshu] += 1
+            if polityka == 'lru':
+                kesh.move_to_end(klyuch_keshu)
             return kesh[klyuch_keshu]
 
         rezultat = funktsiya(*arhy, **kvarhy)
 
+        _zastosuvatyEviktsiynu()
+
         kesh[klyuch_keshu] = rezultat
         lichylnyk_zvertanj[klyuch_keshu] = 1
+        chas_stvorennya[klyuch_keshu] = time.time()
 
         return rezultat
 
